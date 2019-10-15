1ce4c70b6967dc600c07592a8b7ba47dee672be2
Bogdan <pinchuk.brus@gmail.com>
2019-10-15 13:29:12 +0300

    Add LesApp0

---
+++ LesApp0/MainForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿using System.Windows.Forms;
+
+namespace LesApp0
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sb = new System.Windows.Forms.StatusStrip();
+            this.toolLb = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.analysis = new System.Windows.Forms.ToolStripMenuItem();
+            this.tree = new System.Windows.Forms.TreeView();
+            this.tbInfo = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.openFD = new System.Windows.Forms.OpenFileDialog();
+            this.help = new System.Windows.Forms.ToolTip(this.components);
+            this.sb.SuspendLayout();
+            this.menu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sb
+            // 
+            this.sb.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolLb});
+            this.sb.Location = new System.Drawing.Point(0, 240);
+            this.sb.Name = "sb";
+            this.sb.Size = new System.Drawing.Size(361, 22);
+            this.sb.TabIndex = 0;
+            this.sb.Text = "statusBar";
+            // 
+            // toolLb
+            // 
+            this.toolLb.Name = "toolLb";
+            this.toolLb.Size = new System.Drawing.Size(56, 17);
+            this.toolLb.Text = "Help info";
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.analysis});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(361, 24);
+            this.menu.TabIndex = 1;
+            this.menu.Text = "menu";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(100, 6);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // analysis
+            // 
+            this.analysis.Enabled = false;
+            this.analysis.Name = "analysis";
+            this.analysis.Size = new System.Drawing.Size(62, 20);
+            this.analysis.Text = "Analysis";
+            this.analysis.Click += new System.EventHandler(this.analysis_Click);
+            // 
+            // tree
+            // 
+            this.tree.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tree.Location = new System.Drawing.Point(0, 0);
+            this.tree.Name = "tree";
+            this.tree.Size = new System.Drawing.Size(105, 216);
+            this.tree.TabIndex = 2;
+            this.help.SetToolTip(this.tree, "Типи наявні у збірці");
+            this.tree.Click += new System.EventHandler(this.tree_Click);
+            // 
+            // tbInfo
+            // 
+            this.tbInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbInfo.Location = new System.Drawing.Point(0, 0);
+            this.tbInfo.Multiline = true;
+            this.tbInfo.Name = "tbInfo";
+            this.tbInfo.Size = new System.Drawing.Size(252, 216);
+            this.tbInfo.TabIndex = 3;
+            this.help.SetToolTip(this.tbInfo, "Детальна інформація про тип");
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tree);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tbInfo);
+            this.splitContainer1.Size = new System.Drawing.Size(361, 216);
+            this.splitContainer1.SplitterDistance = 105;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // openFD
+            // 
+            this.openFD.FileName = "OpenFD";
+            this.openFD.Filter = "Files *.exe, *.dll|*.exe;*.dll|Все файлы|*.*";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 262);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.sb);
+            this.Controls.Add(this.menu);
+            this.MainMenuStrip = this.menu;
+            this.Name = "MainForm";
+            this.Text = "LesApp0";
+            this.sb.ResumeLayout(false);
+            this.sb.PerformLayout();
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip sb;
+        private System.Windows.Forms.ToolStripStatusLabel toolLb;
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem analysis;
+        private System.Windows.Forms.TreeView tree;
+        private System.Windows.Forms.TextBox tbInfo;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.OpenFileDialog openFD;
+        private ToolTip help;
+    }
+}
+
---
+++ LesApp0/MainForm.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LesApp0
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Шлях до збірки
+        /// </summary>
+        private string path = string.Empty;
+        /// <summary>
+        /// Збірка
+        /// </summary>
+        private Assembly assembly = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Відкриття файлу
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFD.ShowDialog() == DialogResult.OK)
+            {
+                // присвоєння шляху/повної назви файла
+                path = openFD.FileName;
+
+                if (!(Path.GetExtension(path) == ".exe" ||
+                    Path.GetExtension(path) == ".dll"))
+                {
+                    // якщо завантажено файл але не відповідного розширення
+                    toolLb.Text = "File loaded unsuccessfully.";
+                    path = string.Empty;
+                    analysis.Enabled = false;
+                    return;
+                }
+
+                // якщо завантажено файл з правильним розширенням
+                toolLb.Text = $"File loaded: {Path.GetFileName(path)}";
+                analysis.Enabled = true;
+
+
+            }
+            else
+            {
+                // якщо натиснута відміна
+                toolLb.Text = "File wasn't loaded.";
+            }
+        }
+
+        /// <summary>
+        /// Завантаження збірки
+        /// </summary>
+        private void LoadLib()
+        {
+            try
+            {
+                assembly = Assembly.LoadFrom(path);
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.Close();
+            }
+        }
+
+        /// <summary>
+        /// Аналіз файла
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void analysis_Click(object sender, EventArgs e)
+        {
+            if (path == null || path == string.Empty)
+                return;
+
+            // завантаження збірки
+            LoadLib();
+
+            // виведення інформації про всі типи у збірці
+            Type[] types = assembly.GetTypes();
+
+            // Очищення вузлів
+            tree.Nodes.Clear();
+
+            // назва збірки
+            /*dynamic atr = assembly.GetCustomAttribute(typeof(AssemblyTitleAttribute));
+            TreeNode node = new TreeNode($"{atr.Title}");
+            tree.Nodes.Add(node);*/
+
+            // створення нодів/вузлів
+            TreeNode[] nodes = new TreeNode[types.Length];
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                nodes[i] = new TreeNode($"{types[i].Name}");
+            }
+            tree.Nodes.AddRange(nodes);
+        }
+
+        /// <summary>
+        /// При виборі якогось із вузлів
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tree_Click(object sender, EventArgs e)
+        {
+            // Аналіз вибраного вузла
+            TreeNode tn = tree.GetNodeAt(tree.PointToClient(MousePosition));
+
+            // Очищення інформаційного вікна
+            tbInfo.Text = string.Empty;
+
+            // вибраний тип
+            Type type = null;
+
+            // вибір вказаного типу
+            foreach (Type i in assembly.GetTypes())
+            {
+                if (i.Name == tn.Text)
+                {
+                    type = i;
+                    break;
+                }
+            }
+
+            tbInfo.Text += $"Тип: {type?.Name}" + Environment.NewLine;
+
+            #region Класифікація типу
+            tbInfo.Text += $"Класифікація типу:";
+            if (type.IsAbstract) tbInfo.Text += $" абстрактний,";
+            if (type.IsArray) tbInfo.Text += $" масив,";
+            if (type.IsClass) tbInfo.Text += $" клас,";
+            if (type.IsEnum) tbInfo.Text += $" перелік,";
+            if (type.IsInterface) tbInfo.Text += $" інтерфейс,";
+            if (type.IsNested) tbInfo.Text += $" вкладений тип,";
+            if (type.IsSealed) tbInfo.Text += $" закритий від наслідування,";
+            if (type.IsValueType) tbInfo.Text += $" структура,";
+            tbInfo.Text = tbInfo.Text.TrimEnd(',') + ";" + Environment.NewLine + Environment.NewLine;
+            #endregion
+
+            // отримання конструкторів
+            if (type.GetConstructors().Length > 0)
+            {
+                tbInfo.Text += $"Конструктори:" + Environment.NewLine;
+                foreach (ConstructorInfo i in type.GetConstructors())
+                {
+                    tbInfo.Text += $"\t{i.Name}" + Environment.NewLine;
+                }
+            }
+
+        }
+    }
+}
